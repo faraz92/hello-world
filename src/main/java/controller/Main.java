8d59a396f6458d9aca5e77842f1d671a6e7c7939
faraz92 <ftahir@tssg.org>
2016-11-17 14:07:56 +0000

    test jenkinsss

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("hello world jenkin");
+			System.out.println("hello world jenkinsss");
 	}
 
 	
