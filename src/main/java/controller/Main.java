c48ae64243e50da00b7c1490289d654f325963df
faraz92 <ftahir@tssg.org>
2016-11-17 14:16:30 +0000

    Test 4 failure

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,14 +1,14 @@
 package controller;
 
-public class {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello world jenkinsss");
-			System.out.println("hello world jenkinsss");
-			System.out.println("hello world jenkinsss");
-			System.out.println("hello world jenkinsss");
-			System.out.println("hello world jenkinsss");
+			System.out.println("hello world jenkinsss")
+			System.out.println("hello world jenkinsss")
+			System.out.println("hello world jenkinsss")
+			System.out.println("hello world jenkinsss")
 	}
 
 	
