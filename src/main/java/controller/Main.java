961ae52f599841e5c4b0d4e13251a07640b11922
faraz92 <ftahir@tssg.org>
2016-11-17 14:10:10 +0000

    Third Test jenkin

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello world jenkinsss");
+			System.out.println("hello world jenkinsss");
+			System.out.println("hello world jenkinsss");
+			System.out.println("hello world jenkinsss");
+			System.out.println("hello world jenkinsss");
 	}
 
 	
